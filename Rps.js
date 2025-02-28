bb9473d11efff1b7d790b401007cd59ce464f7a4
Sung <25SungC@badinhs.org>
2025-02-28 13:25:35 -0500

    Added Index and Rps Random number

---
+++ Rps.js
@@ -0,0 +1,13 @@
+/* 
+1. Set up a random number generater
+2. Limit the number to 1, 2, or 3
+3. Assign 1 to Rock, 2 to Paper, 3 to Scissors
+4. Set up a prompt that only accepts Rock paper or Scissors
+5. Input Rock beats Scissors, Paper beats Rock, Scissors beats Paper, or Draw
+6. Declare winner
+*/
+
+function randomLessThan(num) {
+    return((Math.floorMath.random()))
+}
+console.log(randomLessThan(3));
