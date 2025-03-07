abd32325293fc4357d4b2b2eca734ef6eff136d0
Sung <25SungC@badinhs.org>
2025-03-07 13:32:45 -0500

    Moving around the code to see if this work

--- Rps.js
+++ Rps.js
@@ -8,28 +8,105 @@
 */
 
 function randomlessthan(num){
-    return(Math.floor(Math.random() * num));
-}
+    return (Math.floor(Math.random() * num))};
 
-// let
+   
 
 let player;
 
+let ScoreboardPlayer = 0 
+let ScoreboardComputer = 0
+
+// while (!player == "ROCK" || !player == "PAPER" || !player == "SCISSORS"){
+//     prompt("Rock, Paper, or Scissors");
+// };
 player = prompt("Rock, Paper, or Scissors?");
 console.log(`Player choose ${player}`);
+player = player.toUpperCase()
 
+let choice = (randomlessthan(3) + 1)
+if (choice == 1){
+    console.log("Computer choose Rock")
+}
+if (choice == 2){
+    console.log("Computer choose Paper")
+}
+if (choice == 3){
+    console.log("Computer choose Scissor")
+}
+while (ScoreboardComputer < 5 || ScoreboardPlayer < 5){
+    if (player == "ROCK" && choice == 3) {
+        console.log("Player won. Rock beats scissors")
+        ScoreboardPlayer = ScoreboardPlayer + 1;
+        console.log(ScoreboardPlayer);
+    }
+    
+    if (player == "ROCK" && choice == 2) {
+        console.log("CPU won. Paper beats Rock")
+        ScoreboardComputer = ScoreboardComputer + 1;
+        console.log(ScoreboardComputer + 1 );
+    }
+    
+    if (player == "ROCK" && choice == 1) {
+        console.log("Tied. Rock on Rock")
+        console.log(ScoreboardPlayer); 
+        console.log(ScoreboardComputer);
+    }
+    
+    if (player == "PAPER" && choice == 1) {
+        console.log("Player won. Paper beats Rock")
+        ScoreboardPlayer = ScoreboardPlayer + 1;
+        console.log(ScoreboardPlayer);
+    }
+    
+    if (player == "PAPER" && choice == 2) {
+        console.log("Tied. Paper on Paper")
+        console.log(ScoreboardPlayer); 
+        console.log(ScoreboardComputer);
+    }
+    
+    if (player == "PAPER" && choice == 3) {
+        console.log("CPU won. Scissors beats Paper")
+        ScoreboardComputer = ScoreboardComputer + 1;
+        console.log(ScoreboardComputer);
+    }
+    
+    if (player == "SCISSORS" && choice == 3) {
+        console.log("Tied. Scissors on Scissors");
+        console.log(ScoreboardPlayer);
+        console.log(ScoreboardComputer);
+    }
+    
+    if (player == "SCISSORS" && choice == 2) {
+        console.log("Player won. Scissors beats Paper")
+        ScoreboardPlayer = ScoreboardPlayer + 1;
+        console.log(ScoreboardPlayer);
+    }
+    
+    if (player == "SCISSORS" && choice == 1) {
+        console.log("CPU won. Rock beats scissors")
+        ScoreboardComputer = ScoreboardComputer + 1;
+        console.log(ScoreboardComputer);
+    }
+    continue;
+    }
 
-if (player == "Rock") {
-    console.log(randomlessthan(3) + 1);
+if (player == "ROCK") {
+    console.log(choice);
 };
 
-if (player == "Paper") {
-    console.log(randomlessthan(3) + 1);
+if (player == "PAPER") {
+    console.log(choice);
 };
 
-if (player == "Scissors") {
-    console.log(randomlessthan(3) + 1);
+if (player == "SCISSORS") {
+    console.log(choice);
 };
-if (!player == "Rock, Paper, Scissors") {
-    prompt("Rock, Paper, or Scissors");
-};
+
+//END LOOP WHEN EITHER PLAYER GETS 5
+if (ScoreboardPlayer >= 5) {
+    alert("Player has won")
+}
+if (ScoreboardComputer >= 5) {
+    alert("You lost to predetermined moves from a BOT. SKILL ISSUE")
+}
